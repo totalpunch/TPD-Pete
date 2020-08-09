9b98dbfa8a04b356ee4d6d22f40e15cc5fabc652
martijnwiekens <14075611+mwiekens@users.noreply.github.com>
2020-08-09 11:38:44 +0200

    Added Amplify deployment

--- tpd_pete/actions/deploymentaction.py
+++ tpd_pete/actions/deploymentaction.py
@@ -2,6 +2,7 @@ import os
 
 from .iaction import IAction
 from .deployment.cloudformationdeployment import CloudFormationDeployment
+from .deployment.amplifydeployment import AmplifyDeployment
 from .deployment.ideploymentaction import EnvironmentEnum
 
 
@@ -24,6 +25,10 @@ class DeploymentAction(IAction):
 		# Check if there is an template
 		if os.path.exists("template.yaml") is True:
 			CloudFormationDeployment().start(**kwargs)
+
+		# Check if there is an amplify folder
+		elif os.path.exists("amplify") is True:
+			AmplifyDeployment().start(**kwargs)
 
 		else:
 			raise Exception("Cant find CloudFormation template: 'template.yaml'")
