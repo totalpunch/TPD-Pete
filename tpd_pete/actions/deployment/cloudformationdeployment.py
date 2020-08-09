95bdc6c2ecc2f5db20400bc6fb6a61e4c7955bb5
martijnwiekens <14075611+mwiekens@users.noreply.github.com>
2020-08-09 11:37:23 +0200

    Opspliten van CloudFormation

--- tpd_pete/actions/deployment/cloudformationdeployment.py
+++ tpd_pete/actions/deployment/cloudformationdeployment.py
@@ -1,40 +1,22 @@
+
 import json
 import os
 import subprocess
 import sys
 import time
-import tempfile
-from enum import Enum
 
 from halo import Halo
 from PyInquirer import prompt
 from termcolor import cprint as print
 
-from .iaction import IAction
 from ..tools.configuration import ConfigurationTool, ConfigKey, ConfigType
 from ..tools.template import TemplateTool
+from .ideploymentaction import IDeploymentAction, EnvironmentEnum
 
 
-class EnvironmentEnum(Enum):
-	DEVELOPMENT = "development"
-	PRODUCTION = "production"
-
-	def __str__(self):
-		return self.value
-
-
-class DeploymentAction(IAction):
-	""" The deployment action
+class CloudFormationDeployment(IDeploymentAction):
+	""" Deployment through AWS CloudFormation
 	"""
-
-	def __init__(self):
-		""" Init the action
-		"""
-		# Initialize the parent
-		super().__init__()
-
-		# Set the default environment to development
-		self.environment = EnvironmentEnum.DEVELOPMENT
 
 	def start(self, **kwargs):
 		""" Start the deployment
@@ -92,19 +74,6 @@ class DeploymentAction(IAction):
 			else:
 				spinner.fail()
 
-	def _createTempDir(self):
-		""" Create a temporary directory for deployment
-
-			Returns path
-		"""
-		# Create the directory
-		self.location = tempfile.mkdtemp()
-
-		# Copy all the current files to it
-		subprocess.check_call("cp -R %s %s" % (".", self.location), shell=True)
-
-		return self.location
-
 	def _createTemporaryTemplate(self):
 		""" Create a temporary template
 
@@ -139,27 +108,6 @@ class DeploymentAction(IAction):
 		parameters['projectName'] = ConfigurationTool.getConfig(ConfigKey.STACK_NAME)
 
 		return parameters
-
-	def _checkDependencies(self):
-		""" Check which dependencies we should install
-		"""
-		# Check NodeJS 'package.json'
-		if os.path.exists("package.json") is True:
-			subprocess.check_call("npm install --production --ignore-scripts --no-audit --prefix %s" % self.location, shell=True)
-
-		# Check Python 'requirements.txt'
-		if os.path.exists("requirements.txt") is True:
-			subprocess.check_call("pip install -r requirements.txt -t %s" % self.location, shell=True)
-
-		# Check Python 'poetry.lock'
-		if os.path.exists("poetry.lock") is True:
-			subprocess.check_call("poetry install", shell=True)
-			os.environ["VIRTUAL_ENV"] = os.popen("poetry env info -p").read()
-
-		# Check Python virtualenv
-		if os.getenv("VIRTUAL_ENV") is not None:
-			packagesPath = os.path.join(os.getenv("VIRTUAL_ENV"), "lib", "python%i.%i" % (sys.version_info[0], sys.version_info[1]), "site-packages")
-			subprocess.check_call("cp -R %s/. %s/" % (packagesPath, self.location), shell=True)
 
 	def _zipContent(self):
 		""" Zip the current directory
